b2fda9b91ca2e204747afae87a91cfa25de027bd
Nilesh Pahari <nileshpahari@protonmail.com>
2024-08-03 21:45:22 +0530

    remove alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
 
 
-// temp
-alert("This website is currently under construction. Some features like mobile view and dark mode color support are not ready yet. Please view this on a laptop/desktop in light(normal) mode")

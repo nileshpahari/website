04ac3da43ee6f93c467eab90a5a2478e847ff144
Nilesh Pahari <nileshpahari@protonmail.com>
2024-08-04 01:36:34 +0530

    dirty navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+let show = false;
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
 
+const nav = document.getElementById("nav");
 
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+
+})
+
+const sidebarIcon = document.querySelector('#sidebar-icon');
+
+sidebarIcon.addEventListener("click", () => {
+    if (show) {
+        document.querySelector("#sidebar").classList.remove("hidden");
+    show = false;
+    }
+    else {
+        document.querySelector("#sidebar").classList.add("hidden");
+    show = true;
+    }
+})
+// closeIcon.addEventListener("click", () => {
+//     document.querySelector('#sidebar').classList.add("hidden")
+// })

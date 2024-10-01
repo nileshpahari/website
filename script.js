f1b51271d0956e77f0c366266a7abfdfb64d2aae
Nilesh Pahari <nileshpahari@protonmail.com>
2024-10-01 17:39:13 +0530

    remove alert

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Responsiveness alert
-alert("⚠️ Website under constuction.\nIf you find any bugs/responsiveness error please mention you device width (if possible model too) and send me a mssg through the contact form.")
+// Logging repo
+console.log("Wanna see all the files?\nCheck the repo itself: https://github.com/nileshpahari/website.git");
 
 // Copy write date 
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
